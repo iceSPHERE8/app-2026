f6eb1666a6b0f115800b70226741de91a705381d
fatoooony <jacknasppear@gmail.com>
2026-06-04 03:55:23 +0800

    keeping...

--- scripts/hash.js
+++ scripts/hash.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcryptjs');
 
-const plainPassword = "明文密码"; // 替换成你想设置的密码
+const plainPassword = "12345678"; // 替换成你想设置的密码
 const saltRounds = 12; // 加密强度
 
 bcrypt.hash(plainPassword, saltRounds, (err, hash) => {
